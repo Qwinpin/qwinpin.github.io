1a95b279110ae46249972ed5d3338ee8b07a857a
Unknown <qwinpin@gmail.com>
2017-09-22 19:12:49 +0400

    Realy greate update:   New features   New disign   New u

--- hw1/js/index.js
+++ hw1/js/index.js
@@ -19,6 +19,7 @@ function changeQ(id){
 var show = false;
 var sorted = false;
 var load_JSON = false;
+var authors_upd = true;
 
 function shuffleArray(array) {
     for (var i = array.length - 1; i > 0; i--) {
@@ -42,32 +43,27 @@ function add_quote(id){
     show = false;
     var button_text = document.getElementById('button_load');
     button_text.innerHTML = "<p>Show qoutes</p>";
-    document.getElementById('button_sort').style.visibility = 
-      'hidden';
-    list_show = [];
-    
-  } else{
-      var new_div = document.getElementById("change");
-      for (var i = 0; i < 3; i++){
-        var bq = document.createElement("blockquote");
-        var para = document.createElement("p");
-        para.className = 'quote';
-        
-        num_of_quote = Math.floor(Math.random() * list_of_quotes.length);
-        list_show.push(list_of_quotes[num_of_quote]);
-                       
-        var text = document.createTextNode(list_of_quotes[num_of_quote]);
-        para.appendChild(text);
-        bq.appendChild(para);
-        new_div.appendChild(bq);
-      }
-      show = true;
-      sorted = false;
-      var button_text = document.getElementById('button_load');
-      button_text.innerHTML = "<p>Hide qoutes</p>";
-        document.getElementById('button_sort').style.visibility =
-          'visible';
+    //document.getElementById('button_sort').style.visibility = 
+    //  'hidden';
+    list_show = [];  
   }
+  var new_div = document.getElementById("change");
+  console.log(prepare_list());
+  for (var i = 0; i < list_show.length; i++){
+    var bq = document.createElement("blockquote");
+    var para = document.createElement("p");
+    para.className = 'quote';
+    var text = document.createTextNode(list_show[i]);
+    para.appendChild(text);
+    bq.appendChild(para);
+    new_div.appendChild(bq);
+  }
+  show = true;
+  sorted = false;
+  var button_text = document.getElementById('button_load');
+  button_text.innerHTML = "<p>Update quotes</p>";
+    document.getElementById('button_sort').style.visibility =
+      'visible';
 }
 
 function sort(id){
@@ -108,6 +104,8 @@ function get_JSON(){
             + text[i].quoteAuthor);
         }
         load_JSON = true;
+        authors_upd = false;
+        update_select();
         document.getElementById("get_JSON").innerHTML = 
           "<p> New loaded </p>";
         document.getElementById("get_JSON").className = 
@@ -118,4 +116,70 @@ function get_JSON(){
     xhttp.open("GET", "https://raw.githubusercontent.com/4skinSkywalker/Database-Quotes-JSON/master/quotes.json", true);
     xhttp.send();
   }
+}
+
+function prepare_list(){
+  var author = "All";
+  select = document.getElementById("selector");
+  author = select.options[select.selectedIndex].value;
+  if (author == 'All'){
+    var number_chosen = [];
+    for(var i = 0; i < 3; i++){
+      do{
+        num_of_quote = Math.floor(Math.random() * list_of_quotes.length);
+      } while (number_chosen.indexOf(num_of_quote) !== -1);
+      number_chosen.push(num_of_quote);
+      list_show.push(list_of_quotes[num_of_quote]);
+    }
+  } else {
+      var newq = [];
+      for (var i = 0; i < list_of_quotes.length; i++){
+        if (list_of_quotes[i].search(author) !== -1){
+          newq.push(list_of_quotes[i]);
+        }
+      }
+      var len = newq.length;
+      if (len > 3){
+        len = 3;
+      }
+      for(var i = 0; i < len; i++){
+        var number_chosen = [];
+        do{
+          num_of_quote = Math.floor(Math.random() * newq.length);
+        } while (number_chosen.indexOf(num_of_quote) !== -1);
+        number_chosen.push(num_of_quote);
+        list_show.push(newq[num_of_quote]);
+      }
+  }
+}
+
+function update_select(){
+  if (authors_upd == false){
+    var select = document.getElementById("selector");
+    var parser = /- [^0-9]+/;
+    var list_of_names = [];
+    for (var i = 0; i < list_of_quotes.length; i++){
+      var name = (parser.exec(list_of_quotes[i]));
+      if (name != null){
+        list_of_names.push(name);
+      }
+    }
+    list_of_names.sort();
+    list_of_names = unique(list_of_names);
+    authors_upd = true;
+    for (var i = 0; i < list_of_names.length; i++){
+      option = document.createElement('option');
+      option.value = option.text = list_of_names[i];
+      select.add( option );
+    }
+  }
+}
+
+function unique(arr) {
+  var obj = {};
+  for (var i = 0; i < arr.length; i++) {
+    var str = arr[i];
+    obj[str] = true;
+  }
+  return Object.keys(obj);
 }
